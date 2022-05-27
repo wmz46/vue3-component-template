84a5d9dfac4617a1d36b1d4db54cc43a499d4a79
钢翼 <659240788@qq.com>
2022-05-27 10:31:00 +0800

    新增element-plus分支

--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,12 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './assets/index.css'
 
-createApp(App).mount('#app')
+import ElementPlus from 'element-plus'
+import zhCn from 'element-plus/es/locale/lang/zh-cn'
+import 'element-plus/dist/index.css'
+import 'element-plus/theme-chalk/display.css'
+const app = createApp(App)
+app.use(ElementPlus,{
+  locale: zhCn
+})
+app.mount('#app')
