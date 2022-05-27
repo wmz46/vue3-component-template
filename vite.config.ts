a7cc5d9ac7e79588c25cdcb9b2e65e9771944808
钢翼 <659240788@qq.com>
2022-05-27 18:10:16 +0800

    element-plus降级，打包支持nomodule

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,14 @@ import postcssImport from 'postcss-import'
 import tailwindcss from 'tailwindcss'
 import autoprefixer from 'autoprefixer'
 import nesting from 'tailwindcss/nesting'
+import legacy from '@vitejs/plugin-legacy'
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(),
+    legacy({
+      additionalLegacyPolyfills: ['regenerator-runtime/runtime'] // 兼容旧版本浏览器
+    })
+  ],
   css:{
     postcss:{
       plugins:[
