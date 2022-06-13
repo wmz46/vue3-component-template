8ac0ed19bf54323d48d7519fca98c57aa352a3bb
钢翼 <659240788@qq.com>
2022-06-13 09:44:32 +0800

    优化日期组件

--- src/components/GeDatePicker.vue
+++ src/components/GeDatePicker.vue
@@ -1,5 +1,5 @@
 <script lang="ts" setup >
-import { computed,reactive } from 'vue'
+import { computed,reactive,watch } from 'vue'
 import dayjs from 'dayjs'
 
 const props = withDefaults(defineProps<{
@@ -15,15 +15,18 @@ const data = reactive({
   month:NaN,
   day:NaN
 })
-if(typeof props.modelValue == 'string') {
-  data.year = parseInt(`${props.modelValue?.split('-')?.[0]}`)
-  data.month = parseInt(`${props.modelValue?.split('-')?.[1]}`)
-  data.day = parseInt(`${props.modelValue?.split('-')?.[2]}`)
-}else if(props.modelValue instanceof Date) {
-  data.year = props.modelValue.getFullYear()
-  data.month = props.modelValue.getMonth() + 1
-  data.day = props.modelValue.getDate()
+const init = () => {
+  if(typeof props.modelValue == 'string') {
+    data.year = parseInt(`${props.modelValue?.split('-')?.[0]}`)
+    data.month = parseInt(`${props.modelValue?.split('-')?.[1]}`)
+    data.day = parseInt(`${props.modelValue?.split('-')?.[2]}`)
+  }else if(props.modelValue instanceof Date) {
+    data.year = props.modelValue.getFullYear()
+    data.month = props.modelValue.getMonth() + 1
+    data.day = props.modelValue.getDate()
+  }
 }
+init()
 const yearOptions: any[] = []
 for (let y = new Date().getFullYear() + 10; y >= 1700; y--) {
   yearOptions.push({
@@ -31,6 +34,9 @@ for (let y = new Date().getFullYear() + 10; y >= 1700; y--) {
     label: `${y}`
   })
 }
+watch(() => props.modelValue,() => {
+  init()
+})
 const maxDay = computed(() => {
   let max = 30
   if ([1,3,5, 7, 8, 10, 12].indexOf(data.month) > -1) {
@@ -56,13 +62,17 @@ const changeHandle = () => {
 
 </script>
 <template>
-  <el-select-v2 v-model='data.year' :options='yearOptions' :filterable='filterable' allow-create @change='changeHandle' /> 年
-  <el-select v-model='data.month' @change='changeHandle'>
-    <el-option v-for='i of 12' :key='i' :label='i' :value='i' />
-  </el-select> 月
-  <el-select v-model='data.day' @change='changeHandle'>
-    <el-option v-for='i of maxDay' :key='i' :lable='i' :value='i' />
-  </el-select> 日
+  <div v-bind='$attr'>
+    <el-select v-model='data.year' :filterable='filterable' allow-create @change='changeHandle'>
+      <el-option v-for='item in yearOptions' :key='item.value' :label='item.label' :value='item.value' />
+    </el-select> 年
+    <el-select v-model='data.month' @change='changeHandle'>
+      <el-option v-for='i of 12' :key='i' :label='i' :value='i' />
+    </el-select> 月
+    <el-select v-model='data.day' @change='changeHandle'>
+      <el-option v-for='i of maxDay' :key='i' :lable='i' :value='i' />
+    </el-select> 日
+  </div>
 </template>
 <style scoped>
 </style>
