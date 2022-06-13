b5829f99a2dfb4e6cff642fbb215c1e568807647
钢翼 <659240788@qq.com>
2022-06-13 10:10:52 +0800

    优化日期组件

--- src/components/GeDatePicker.vue
+++ src/components/GeDatePicker.vue
@@ -31,7 +31,7 @@ const yearOptions: any[] = []
 for (let y = new Date().getFullYear() + 10; y >= 1700; y--) {
   yearOptions.push({
     value: y,
-    label: `${y}`
+    label: `${y}年`
   })
 }
 watch(() => props.modelValue,() => {
@@ -62,16 +62,17 @@ const changeHandle = () => {
 
 </script>
 <template>
-  <div v-bind='$attr'>
-    <el-select v-model='data.year' :filterable='filterable' allow-create @change='changeHandle'>
+  <div v-bind='$attrs'>
+    <el-select v-model='data.year' :filterable='filterable' allow-create class='w-40 mr-3' @change='changeHandle'>
       <el-option v-for='item in yearOptions' :key='item.value' :label='item.label' :value='item.value' />
-    </el-select> 年
-    <el-select v-model='data.month' @change='changeHandle'>
-      <el-option v-for='i of 12' :key='i' :label='i' :value='i' />
-    </el-select> 月
-    <el-select v-model='data.day' @change='changeHandle'>
-      <el-option v-for='i of maxDay' :key='i' :lable='i' :value='i' />
-    </el-select> 日
+    </el-select>
+    <el-select v-model='data.month' class='w-40 mr-3' @change='changeHandle'>
+      <el-option v-for='i of 12' :key='i' :label='i+"月"' :value='i' />
+    </el-select>
+    <el-select v-model='data.day' class='w-40' @change='changeHandle'>
+      <el-option v-for='i of maxDay' :key='i' :label='i+"日"' :value='i' />
+    </el-select>
+
   </div>
 </template>
 <style scoped>
